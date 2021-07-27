381d9cffc88303d610b0160da3a3a59126e1bc3e
johnebah <70802507+johnebah@users.noreply.github.com>
2021-07-27 02:00:48 +0100

    Delete jcode directory

--- jcode/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_destroy();
-header("location:admin_login.php");
-?>
--- jcode/php/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = mysqli_connect("remotemysql.com", "4KhahQocon", "CtqaizNxko", "4KhahQocon");
-    if (!$con) {
-        echo "No database found";
-    }
-?>
