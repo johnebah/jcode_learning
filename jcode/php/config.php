dedc740d33578b0d4611976256755c17cb8deca0
johnebah <70802507+johnebah@users.noreply.github.com>
2021-07-27 01:31:23 +0100

    Update config.php

--- jcode/php/config.php
+++ jcode/php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "jcode");
+    $con = mysqli_connect("remotemysql.com", "4KhahQocon", "CtqaizNxko", "4KhahQocon");
     if (!$con) {
         echo "No database found";
     }
