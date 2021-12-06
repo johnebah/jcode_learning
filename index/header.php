592c4644c2f466e55ee20c8a7336204b17185342
johnebah <70802507+johnebah@users.noreply.github.com>
2021-12-06 14:57:24 +0100

    Add files via upload

---
+++ index/header.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-white">
+                                <div class="container-fluid navigation">           
+                                <a class="navbar-brand" style="color:white;font-weight:bolder;"><img src="juju.png"></a>
+                                <button style="background-color: black;" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" arial-controls="navbarTogglerDemo02" arial-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                                        <ul class="navbar-nav ml-auto">
+                                            <li class="nav-item active"><a class="nav-link" href="#" style="color: black;">Home</a></li>
+                                            <li class="nav-item"><a class="nav-link" href="#" style="color: black;">Orders</a></li>
+                                            <li class="nav-item"><a class="nav-link" href="#" style="color: black;">About</a></li>
+                                            <li class="nav-item"><a class="nav-link" href="#" style="color: black;">Info</a></li>
+                                        </ul>
+                                </div>
+                                </div>
+</nav>
