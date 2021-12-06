351003cc5fddf83854051e219977439fd0e3efae
johnebah <70802507+johnebah@users.noreply.github.com>
2021-12-06 15:23:01 +0100

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("remotemysql.com", "4KhahQocon", "7xFcs5hKF3", "4KhahQocon");
+    $con = mysqli_connect("remotemysql.com:3306", "4KhahQocon", "7xFcs5hKF3", "4KhahQocon");
     if ($con) {
         echo "<script>alert('No database found')</script>";
     }
